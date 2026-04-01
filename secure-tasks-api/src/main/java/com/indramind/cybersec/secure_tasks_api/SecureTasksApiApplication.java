6bc8318493e78d81d5971cdf17d437c99af292bb
Christian <christian.most@alumnos.upm.es>
2026-04-01 02:46:31 +0200

    Initial commit

---
+++ secure-tasks-api/src/main/java/com/indramind/cybersec/secure_tasks_api/SecureTasksApiApplication.java
@@ -0,0 +1,13 @@
+package com.indramind.cybersec.secure_tasks_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTasksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTasksApiApplication.class, args);
+	}
+
+}
---
+++ secure-tasks-api/src/test/java/com/indramind/cybersec/secure_tasks_api/SecureTasksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indramind.cybersec.secure_tasks_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTasksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
